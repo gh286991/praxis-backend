b64dd2f0c7642a026659a52b380aa6447e5a0d6d
gh286991 <gh286991@gmail.com>
2026-01-21 16:09:16 +0800

    根據環境條件性地設定 `sameSite` cookie 選項。

--- src/config/cookie.config.ts
+++ src/config/cookie.config.ts
@@ -3,6 +3,6 @@ import { CookieOptions } from 'express';
 export const COOKIE_OPTIONS: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   maxAge: 24 * 60 * 60 * 1000, // 1 day
 };
